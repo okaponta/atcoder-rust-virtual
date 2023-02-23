aeff7b32c6c37ac79096cafd08f0e98a4ad80ca3
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-02-23 22:41:36 +0900

    add 5cfc34ef-fa52-44dc-a012-425e8abe0431_2

--- 5cfc34ef-fa52-44dc-a012-425e8abe0431/src/bin/2.rs
+++ 5cfc34ef-fa52-44dc-a012-425e8abe0431/src/bin/2.rs
@@ -3,6 +3,32 @@ use proconio::input;
 fn main() {
     input! {
         n:usize,
+        k:usize,
+        a:[usize;n],
+        b:[usize;n],
     }
-    println!("{}", n);
+    let mut c = vec![a[0], b[0]];
+    for i in 1..n {
+        let mut next = vec![];
+        for e in c {
+            if abs(e, a[i]) <= k {
+                next.push(a[i]);
+            }
+            if abs(e, b[i]) <= k {
+                next.push(b[i]);
+            }
+        }
+        next.sort();
+        next.dedup();
+        c = next;
+    }
+    println!("{}", if c.len() == 0 { "No" } else { "Yes" });
+}
+
+fn abs(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    } else {
+        a - b
+    }
 }
